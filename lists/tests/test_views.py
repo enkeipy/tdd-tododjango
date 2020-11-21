19b420c48f383acbced027d94399768b70e7da0c
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-20 22:41:12 -0600

    Add validate_unique for Form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -91,16 +91,20 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    # def test_validation_errors_end_up_on_lists_page(self):
-    #     list_ = List.objects.create()
-    #     response = self.client.post(
-    #         f'/lists/{list_.id}/',
-    #         data={'text': ''}
-    #     )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'list.html')
-    #     expected_error = escape("You can't have an empty list item")
-    #     self.assertContains(response, expected_error)
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
